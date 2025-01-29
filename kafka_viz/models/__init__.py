25d076a4dfc5a80ea7dcce695285aa64284d0bda
Emil Holmegaard <emilholmegaard@gmail.com>
2025-01-29 16:02:52 +0100

    Export Service and KafkaTopic classes in models namespace

--- kafka_viz/models/__init__.py
+++ kafka_viz/models/__init__.py
@@ -1,3 +1,14 @@
 """
 Data models for representing Kafka services and topics
 """
+
+from .service import Service
+from .schema import KafkaTopic, Schema, AvroSchema, DTOSchema
+
+__all__ = [
+    'Service',
+    'KafkaTopic',
+    'Schema',
+    'AvroSchema',
+    'DTOSchema',
+]
