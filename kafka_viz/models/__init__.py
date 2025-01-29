d1cb03da942a059f4f2136bc1f9779887f923268
Emil Holmegaard <emilholmegaard@gmail.com>
2025-01-29 16:00:59 +0100

    Add models package __init__.py

--- kafka_viz/models/__init__.py
+++ kafka_viz/models/__init__.py
@@ -1,13 +1,3 @@
-"""Models for representing Kafka-based architectures."""
-
-from .service import Service, ServiceCollection
-from .schema import Schema, AvroSchema, DTOSchema, KafkaTopic
-
-__all__ = [
-    'Service',
-    'ServiceCollection',
-    'Schema',
-    'AvroSchema',
-    'DTOSchema',
-    'KafkaTopic'
-]
+"""
+Data models for representing Kafka services and topics
+"""
