4d2d0971b1a2e815c73643cf0053c71c4ea8ac47
Emil Holmegaard <emilholmegaard@gmail.com>
2025-01-30 15:29:51 +0100

    Add test_project_path fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,16 +2,6 @@ import pytest
 from pathlib import Path
 
 @pytest.fixture
-def test_data_dir() -> Path:
-    """Fixture providing path to test data directory."""
-    return Path(__file__).parent / 'test_data'
-
-@pytest.fixture
-def python_service_dir(test_data_dir) -> Path:
-    """Fixture providing path to Python test service."""
-    return test_data_dir / 'python_service'
-
-@pytest.fixture
-def spring_service_dir(test_data_dir) -> Path:
-    """Fixture providing path to Spring test service."""
-    return test_data_dir / 'spring_service'
+def test_project_path(test_data_dir):
+    """Directory containing test project data."""
+    return Path(test_data_dir) / "java"
