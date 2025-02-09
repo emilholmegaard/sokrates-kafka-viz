da85b6cb72afc936fbf8da76bf050e4451de35bf
emilholmegaard <emhol@emhol7ndk>
2025-02-09 15:26:59 +0100

    add service name extractors

--- kafka_viz/analyzers/__init__.py
+++ kafka_viz/analyzers/__init__.py
@@ -1,3 +1,10 @@
+from .analyzer_manager import AnalyzerManager
+from .avro_analyzer import AvroAnalyzer
+from .base import BaseAnalyzer, KafkaPatterns
+from .dependency_analyzer import DependencyAnalyzer
+from .java_analyzer import JavaAnalyzer
+from .kafka_analyzer import KafkaAnalyzer
+from .service_analyzer import ServiceAnalyzer
 from .service_name_extractors import (
     CSharpServiceNameExtractor,
     JavaScriptServiceNameExtractor,
@@ -5,8 +12,18 @@ from .service_name_extractors import (
     PythonServiceNameExtractor,
     ServiceNameExtractor,
 )
+from .spring_analyzer import SpringCloudStreamAnalyzer
 
 __all__ = [
+    "AnalyzerManager",
+    "AvroAnalyzer",
+    "BaseAnalyzer",
+    "DependencyAnalyzer",
+    "JavaAnalyzer",
+    "KafkaAnalyzer",
+    "KafkaPatterns",
+    "ServiceAnalyzer",
+    "SpringCloudStreamAnalyzer",
     "ServiceNameExtractor",
     "JavaServiceNameExtractor",
     "JavaScriptServiceNameExtractor",
