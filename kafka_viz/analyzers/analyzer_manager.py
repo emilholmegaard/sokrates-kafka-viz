0022f44ee6429d8c98576895f834f9ff86df98b8
Emil Holmegaard <emilholmegaard@gmail.com>
2025-02-11 16:24:27 +0100

    Add missing Optional import to AnalyzerManager

--- kafka_viz/analyzers/analyzer_manager.py
+++ kafka_viz/analyzers/analyzer_manager.py
@@ -3,7 +3,7 @@
 import json
 import logging
 from pathlib import Path
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Optional
 
 from ..models.schema import KafkaTopic
 from ..models.service import Service
