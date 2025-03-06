86575518ae055f0af65a8890abf1859d158f5c9f
Emil Holmegaard <emilholmegaard@gmail.com>
2025-03-06 07:33:02 +0100

    Update visualization package __init__.py

---
+++ kafka_viz/visualization/__init__.py
@@ -0,0 +1,20 @@
+"""
+Visualization package for Kafka-Viz.
+
+This package contains various visualization generators for displaying
+Kafka communication patterns in different formats.
+"""
+
+from .base import BaseGenerator
+from .factory import visualization_factory
+from .kafka_viz import KafkaViz
+from .mermaid import MermaidGenerator
+from .simple_viz import SimpleViz
+
+__all__ = [
+    'BaseGenerator',
+    'visualization_factory',
+    'KafkaViz',
+    'MermaidGenerator',
+    'SimpleViz',
+]
