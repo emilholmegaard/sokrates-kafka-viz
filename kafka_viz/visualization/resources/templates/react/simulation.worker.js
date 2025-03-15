ee4f60604a06d54031e7b2c3f1a323e0a9b89257
Emil Holmegaard <emilholmegaard@gmail.com>
2025-03-15 08:56:10 +0100

    Simplify worker file since simulation now runs on main thread

--- kafka_viz/visualization/resources/templates/react/simulation.worker.js
+++ kafka_viz/visualization/resources/templates/react/simulation.worker.js
@@ -1,21 +1,3 @@
-self.importScripts('https://d3js.org/d3.v7.min.js');
-
-self.addEventListener('message', function (e) {
-    const { nodes, links, width, height } = e.data;
-
-    const simulation = d3.forceSimulation(nodes)
-        .force("link", d3.forceLink(links).id(d => d.id).distance(50))
-        .force("charge", d3.forceManyBody().strength(-100))
-        .force("center", d3.forceCenter(width / 2, height / 2))
-        .force("collide", d3.forceCollide().radius(30))
-        .alphaDecay(0.05)
-        .alphaMin(0.001);
-
-    simulation.on("tick", () => {
-        self.postMessage({ type: 'tick', nodes, links });
-    });
-
-    simulation.on("end", () => {
-        self.postMessage({ type: 'end', nodes, links });
-    });
-});
+// This file is kept for compatibility but is no longer used
+// The simulation is now running on the main thread to avoid Web Worker issues in local development
+console.log('Simulation worker loaded but not used - simulation running on main thread');
